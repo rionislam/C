f7c6661cdff7fd3291fcdec5b26b11ee6517b048
rionislam <rionislam.mazedul@gmail.com>
2024-09-14 18:02:21 +0600

    Solved 2nd practice question

---
+++ Practice/QS2/qs2.c
@@ -0,0 +1,22 @@
+// Write a program to calculate area of a circle. (Radius given)
+
+#include <stdio.h>
+
+int main()
+{
+    int pie = 3.1416;
+    int rad;
+    printf("Enter radius: ");
+    scanf("%d", &rad);
+    printf("\n");
+
+    if (rad != 0)
+    {
+
+        printf("Area is %d", pie * rad * rad);
+    }
+    else
+    {
+        printf("Area is 0");
+    }
+}
